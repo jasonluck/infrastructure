14737e063d2cc0706d1560a81d89f01db6f26a4b
jasonluck <jluck@meetveracity.com>
2017-12-14 16:58:16 -0500

    Add vault cluster config

--- main.tf
+++ main.tf
@@ -6,8 +6,6 @@
 # ---------------------------------------------------------------------------------------------------------------------
 
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
   region = "${var.aws_region}"
 }
 
@@ -46,7 +44,7 @@ data "aws_ami" "consul" {
   }
 }
 
-module "consul_server" {
+module "consul_cluster" {
   source = "github.com/hashicorp/terraform-aws-consul//modules/consul-cluster?ref=v0.1.0"
 
   vpc_id = "${var.vpc_id}"
@@ -54,6 +52,9 @@ module "consul_server" {
   ssh_key_name = "${aws_key_pair.auth.key_name}"
   ami_id = "${var.consul_ami_id == "" ? data.aws_ami.consul.image_id : var.consul_ami_id}"
   instance_type = "t2.medium"
+
+  # To make testing easier, we allow requests from any IP address here but in a production deployment, we *strongly*
+  # recommend you limit this to the IP address ranges of known, trusted servers inside your VPC.
   allowed_inbound_cidr_blocks = ["0.0.0.0/0"]
   allowed_ssh_cidr_blocks = ["0.0.0.0/0"]
 
@@ -69,6 +70,108 @@ module "consul_server" {
               /opt/consul/bin/run-consul --server --cluster-tag-key consul-cluster --cluster-tag-value development
               EOF
 }
+
+# ---------------------------------------------------------------------------------------------------------------------
+# THE USER DATA SCRIPT THAT WILL RUN ON EACH CONSUL SERVER WHEN IT'S BOOTING
+# This script will configure and start Consul
+# ---------------------------------------------------------------------------------------------------------------------
+
+data "template_file" "user_data_consul" {
+  template = "${file("${path.module}/scripts/user-data-consul.sh")}"
+
+  vars {
+    consul_cluster_tag_key   = "${var.consul_cluster_tag_key}"
+    consul_cluster_tag_value = "${var.consul_cluster_name}"
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# DEPLOY A VAULT CLUSTER IN AWS
+# These templates show an example of how to use the vault-consul-cluster module to deploy Vault in AWS. We deploy two Auto
+# Scaling Groups (ASGs): one with a small number of Consul server nodes and one with a larger number of Consul client
+# nodes. Note that these templates assume that the AMI you provide via the ami_id input variable is built from
+# the [consul-ami](https://github.com/jasonluck/consul-ami) Packer template.
+# ---------------------------------------------------------------------------------------------------------------------
+
+data "aws_ami" "vault_consul" {
+  most_recent      = true
+
+  owners     = ["${var.owner}"]
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["vault-consul-amzn-linux-*"]
+  }
+}
+
+module "vault_cluster" {
+  source = "github.com/hashicorp/terraform-aws-vault.git//modules/vault-cluster?ref=v0.0.8"
+
+  cluster_name  = "vault-dev"
+  cluster_size  = 3
+  instance_type = "t2.medium"
+
+  ami_id    = "${var.vault_ami_id == "" ? data.aws_ami.vault_consul.image_id : var.vault_ami_id}"
+  user_data = "${data.template_file.user_data_vault_cluster.rendered}"
+
+  s3_bucket_name          = "${var.s3_bucket_name}"
+  force_destroy_s3_bucket = "${var.force_destroy_s3_bucket}"
+
+  vpc_id     = "${var.vpc_id}"
+  subnet_ids = ["${data.aws_subnet_ids.private.ids}"]
+
+  # Tell each Vault server to register in the ELB.
+  //load_balancers = ["${module.vault_elb.load_balancer_name}"]
+
+  # Do NOT use the ELB for the ASG health check, or the ASG will assume all sealed instances are unhealthy and
+  # repeatedly try to redeploy them.
+  health_check_type = "EC2"
+
+  # To make testing easier, we allow requests from any IP address here but in a production deployment, we *strongly*
+  # recommend you limit this to the IP address ranges of known, trusted servers inside your VPC.
+
+  allowed_ssh_cidr_blocks            = ["0.0.0.0/0"]
+  allowed_inbound_cidr_blocks        = ["0.0.0.0/0"]
+  allowed_inbound_security_group_ids = []
+  ssh_key_name                       = "${aws_key_pair.auth.key_name}"
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ATTACH IAM POLICIES FOR CONSUL
+# To allow our Vault servers to automatically discover the Consul servers, we need to give them the IAM permissions from
+# the Consul AWS Module's consul-iam-policies module.
+# ---------------------------------------------------------------------------------------------------------------------
+
+module "consul_iam_policies_servers" {
+  source = "github.com/hashicorp/terraform-aws-consul.git//modules/consul-iam-policies?ref=v0.1.0"
+
+  iam_role_id = "${module.vault_cluster.iam_role_id}"
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# THE USER DATA SCRIPT THAT WILL RUN ON EACH VAULT SERVER WHEN IT'S BOOTING
+# This script will configure and start Vault
+# ---------------------------------------------------------------------------------------------------------------------
+
+data "template_file" "user_data_vault_cluster" {
+  template = "${file("${path.module}/scripts/user-data-vault.sh")}"
+
+  vars {
+    aws_region               = "${var.aws_region}"
+    s3_bucket_name           = "${var.s3_bucket_name}"
+    consul_cluster_tag_key   = "${var.consul_cluster_tag_key}"
+    consul_cluster_tag_value = "${var.consul_cluster_name}"
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# GENERAL VPC CONFIGURATION
+# ---------------------------------------------------------------------------------------------------------------------
 
 data "aws_subnet_ids" "private" {
   vpc_id = "${var.vpc_id}"
--- variables.tf
+++ variables.tf
@@ -1,30 +1,26 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ENVIRONMENT VARIABLES
+# Define these secrets as environment variables
+# ---------------------------------------------------------------------------------------------------------------------
+
+# AWS_ACCESS_KEY_ID
+# AWS_SECRET_ACCESS_KEY
+
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# You must provide a value for each of these parameters.
+# ---------------------------------------------------------------------------------------------------------------------
 
 variable "vpc_id" {
     description = "ID for the VPC"
-    default     = "vpc-ae6ac2cb"
-}
-
-variable "subnet_ids" {
-    description = "ID for the Utility VPC private subnet"
-    default     = "subnet-025d7a67"
 }
 
 variable "aws_region" {
   description = "AWS region to launch servers."
-  default     = "us-gov-west-1"
-}
-
-variable "access_key" {
-    description = "AWS Access Key"
-}
-
-variable "secret_key" {
-    description = "AWS Secret Key"
 }
 
 variable "owner" {
     description = "AMI Owner"
-    default = "272417811699"
 }
 
 variable "ssh_public_key_path" {
@@ -41,8 +37,62 @@ variable "ssh_key_name" {
   description = "Desired name of AWS key pair"
 }
 
+variable "s3_bucket_name" {
+  description = "The name of an S3 bucket to create and use as a storage backend for Vault. Note: S3 bucket names must be *globally* unique."
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These parameters have reasonable defaults.
+# ---------------------------------------------------------------------------------------------------------------------
 
 variable "consul_ami_id" {
-  description = "Id of the consul AMI to use."
+  description = "Id of the consul AMI to use. If left blank, default to the latest AMI named consul-amzn-linux-*"
   default = ""
+}
+
+variable "vault_ami_id" {
+  description = "Id of the vault AMI to use. If left blank, default to the latest AMI named vault-consul-amzn-linux-*"
+  default = ""
+}
+
+variable "vault_cluster_name" {
+  description = "What to name the Vault server cluster and all of its associated resources"
+  default     = "vault-dev"
+}
+
+variable "consul_cluster_name" {
+  description = "What to name the Consul server cluster and all of its associated resources"
+  default     = "consul-dev"
+}
+
+variable "vault_cluster_size" {
+  description = "The number of Vault server nodes to deploy. We strongly recommend using 3 or 5."
+  default     = 3
+}
+
+variable "consul_cluster_size" {
+  description = "The number of Consul server nodes to deploy. We strongly recommend using 3 or 5."
+  default     = 3
+}
+
+
+variable "vault_instance_type" {
+  description = "The type of EC2 Instance to run in the Vault ASG"
+  default     = "t2.micro"
+}
+
+variable "consul_instance_type" {
+  description = "The type of EC2 Instance to run in the Consul ASG"
+  default     = "t2.micro"
+}
+
+variable "consul_cluster_tag_key" {
+  description = "The tag the Consul EC2 Instances will look for to automatically discover each other and form a cluster."
+  default     = "consul-servers"
+}
+
+variable "force_destroy_s3_bucket" {
+  description = "If you set this to true, when you run terraform destroy, this tells Terraform to delete all the objects in the S3 bucket used for backend storage. You should NOT set this to true in production or you risk losing all your data! This property is only here so automated tests of this module can clean up after themselves."
+  default     = false
 }
